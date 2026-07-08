766913f242ceac631220de8b76a2de866fe361b9
Devansh <dubey.devansh.dev@gmail.com>
2026-07-09 04:08:19 +0530

    Making code cleaner by putting components in separate files

---
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -0,0 +1,38 @@
+import RobotProfileImage from '../assets/robot.png';
+import UserProfileImage from '../assets/user.png';
+
+export function ChatMessage({message,sender}){
+        //const message=props.message;
+        //const {message}=props; SHORTCUT
+        //const sender=props.sender;
+        //const {message,sender}=props;
+        
+
+        /*if(sender==='robot'){
+           return (
+          <div>
+            <img src="robot.png" width="50"></img>
+           {message}
+          </div>
+        );
+        } */
+
+        return (
+          <div className={sender==='user'
+          ?'chat-message-user'
+          : 'chat-message-robot'}>
+
+            {sender==='robot' && (
+              <img src={RobotProfileImage} 
+              className="chat-message-profile"></img>
+            )}
+            <div className="chat-message-text">
+           {message}
+           </div>
+           {sender==='user' &&  (
+              <img src={UserProfileImage} 
+              className="chat-message-profile" ></img> 
+            )}
+          </div>
+        );
+      }
