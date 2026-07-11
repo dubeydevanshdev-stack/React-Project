2c2e9010c5eef8e359db20ef9c2c0bd8556d41b5
Devansh <dubey.devansh.dev@gmail.com>
2026-07-12 03:35:00 +0530

    Removing default messages

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -9,21 +9,9 @@ function App(){
 
         const[chatMessages,setChatMessages]=useState([{
 
-          message:'hello chatbot',
-          sender: 'user',
-          id:'id1'
-        },{
-          message:'Hello! How can I help you',
-          sender: 'robot',
-          id:'id2'
-        },{
-          message:'can you get me todays date',
-          sender: 'user',
-          id:'id3'
-        },{
-          message:'Today is JULY 30',
-          sender: 'robot',
-          id:'id4'
+          message:'',
+          sender: '',
+          id:''
         }]);
         //const [chatMessages,setChatMessages]=array;
         //const chatMessages= array[0];
